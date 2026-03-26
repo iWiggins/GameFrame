d69fdba36f3a6963997dd33f5cf23a8a4f5bc846
Ian Wiggins <ian.a.wiggins919@gmail.com>
2026-03-25 20:49:28 -0500

    added and tested canvas layout

---
+++ GameFrame/Layout/CanvasLayout.cs
@@ -0,0 +1,47 @@
+﻿using GameFrame.Core.Interfaces;
+using Microsoft.Xna.Framework;
+using System.Collections.Generic;
+
+namespace GameFrame.Layout;
+
+/// <summary>
+/// A layout that arranges children in relative positioning.
+/// When resized, the children maintain their relative positions and relative sizes.
+/// </summary>
+/// <param name="parent"></param>
+/// <param name="width"></param>
+/// <param name="height"></param>
+public class CanvasLayout(IComponent? parent = null) : Layout(parent)
+{
+
+	protected override IEnumerable<IComponent> Arrange()
+	{
+		if(_lastGeometry is not null && _children.Count > 0)
+		{
+			double xScale = Width / _lastGeometry.Value.Width;
+			double yScale = Height / _lastGeometry.Value.Height;
+
+			foreach(var child in _children)
+			{
+				if(child is IGeometric geometric)
+				{
+					int dispX = geometric.X - _lastGeometry.Value.X;
+					int dispY = geometric.Y - _lastGeometry.Value.Y;
+					int relX = (int)(dispX * xScale);
+					int relY = (int)(dispY * yScale);
+					int x = X + relX;
+					int y = Y + relY;
+					int width = (int)(geometric.Width * xScale);
+					int height = (int)(geometric.Height * yScale);
+					geometric.Geometry = new(x, y, width, height);
+				}
+			}
+		}
+
+		_lastGeometry = Geometry;
+
+		return Order();
+	}
+
+	private Rectangle? _lastGeometry;
+}
---
+++ TestGameFrame/Layout/CanvasLayoutTests.cs
@@ -0,0 +1,47 @@
+﻿using GameFrame.Layout;
+using TestGameFrame.Utils;
+
+namespace TestGameFrame.Layout;
+public class CanvasLayoutTests() : LayoutTestBase(new CanvasLayout())
+{
+	[Fact]
+	public void CanvasLayoutMaintainsRelativeGeometry()
+	{
+		CanvasLayout layout = new()
+		{
+			X = 2,
+			Y = 2,
+			Width = 10,
+			Height = 10
+		};
+
+		TestGeometric component = new(1)
+		{
+			X = 7,
+			Y = 7,
+			Width = 2,
+			Height = 2
+		};
+
+		Assert.True(layout.AddChild(component));
+
+		layout.ArrangeChildren();
+
+		Assert.Equal(7, component.X);
+		Assert.Equal(7, component.Y);
+		Assert.Equal(2, component.Width);
+		Assert.Equal(2, component.Height);
+
+		layout.X = 7;
+		layout.Y = 7;
+		layout.Width = 20;
+		layout.Height = 20;
+
+		layout.ArrangeChildren();
+
+		Assert.Equal(17, component.X);
+		Assert.Equal(17, component.Y);
+		Assert.Equal(4, component.Width);
+		Assert.Equal(4, component.Height);
+	}
+}
