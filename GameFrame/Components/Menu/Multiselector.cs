c14d071871f7bbad4187f22532dc1878f3607df3
Ian Wiggins <ian.a.wiggins919@gmail.com>
2026-03-27 16:23:17 -0500

    Added Multiselector and NumericUpDown

---
+++ GameFrame/Components/Menu/Multiselector.cs
@@ -0,0 +1,46 @@
+﻿using GameFrame.Core.Clickables;
+using GameFrame.Core.Input;
+using GameFrame.Core.Interfaces;
+using GameFrame.Layout;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Media;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameFrame.Components.Menu;
+public class Multiselector(IEnumerable<IComponent> options, IComponent? parent = null, int layer = 0) :
+	ClickableTwig<FillLayout>(new(), parent, layer), IInitialize
+{
+	public bool Initialized { get; private set; } = false;
+
+	public IComponent CurrentComponent => _options[_current];
+	public int Current => _current;
+
+	public void Advance()
+	{
+		Child.RemoveChild(_options[_current]);
+		_current += 1;
+		if(_current >= _options.Length) _current = 0;
+		Child.AddChild(_options[_current]);
+	}
+
+	public void Initialize()
+	{
+		Child.AddChild(_options[0]);
+
+		Initialized = true;
+	}
+
+	protected override bool OnReleased(Mouse.Buttons button, Point position, double duration)
+	{
+		Advance();
+
+		return true;
+	}
+
+	readonly IComponent[] _options = [.. options];
+	int _current = 0;
+}
---
+++ GameFrame/Components/Menu/NumericUpDown.cs
@@ -0,0 +1,89 @@
+﻿using GameFrame.Components.Buttons;
+using GameFrame.Components.Text;
+using GameFrame.Core.Geometrics;
+using GameFrame.Core.Interfaces;
+using GameFrame.Layout;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace GameFrame.Components.Menu;
+public class NumericUpDown(Texture2D up, Texture2D down, SpriteFont number, int min, int max, int start, IComponent? parent = null, int layer = 0) :
+	GeometricTwig<FlowLayout>(new(FlowLayout.Direction.Down), parent, layer), IInitialize, IReset
+{
+	public bool Initialized { get; private set; } = false;
+
+	public Color TextColor
+	{
+		get => _text.Color;
+		set => _text.Color = value;
+	}
+
+	public Color ButtonColor
+	{
+		get => _up.Color;
+		set
+		{
+			_up.Color = value;
+			_down.Color = value;
+		}
+	}
+
+	public int Value
+	{
+		get => _value;
+		set
+		{
+			_value = value;
+			SetText();
+		}
+	}
+
+	public void Increment()
+	{
+		if(_value < max)
+		{
+			_value += 1;
+			SetText();
+		}
+	}
+
+	public void Decrement()
+	{
+		if(_value > min)
+		{
+			_value -= 1;
+			SetText();
+		}
+	}
+
+	public void Initialize()
+	{
+		Child.AddChild(_up);
+		_up.Pressed += (b,p) => Increment();
+
+		Child.AddChild(_text);
+
+		Child.AddChild(_down);
+		_down.Pressed += (b, p) => Decrement();
+
+		SetText();
+
+		Initialized = true;
+	}
+	public void Reset()
+	{
+		_value = start;
+		SetText();
+	}
+
+	private void SetText() => _text.Contents = _value.ToString();
+
+#pragma warning disable CS9124
+	// start needs to be captured for the Reset functionality, but _value is mutated.
+	// warning is a false positive.
+	private int _value = start;
+#pragma warning restore CS9124
+	private readonly BoundText _text = new(number);
+	private readonly ImageButton _up = new(up);
+	private readonly ImageButton _down = new(down);
+}
